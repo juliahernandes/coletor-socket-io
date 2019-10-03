cae00ddd118c5bb43e7fea8f569faa983c54914e
Julia Hernandes <juliachernandes@gmail.com>
2019-10-03 15:38:42 -0300

    Server

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require('express')(),
+    http = require('http').createServer(app),
+    io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        console.log('message: ', msg);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
