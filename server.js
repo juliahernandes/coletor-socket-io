cda39d0b411d1a5ea413040354554949a9a42006
Julia Hernandes <juliachernandes@gmail.com>
2019-10-04 15:28:10 -0300

    Alert

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('chat message', (msg) => {
         console.log('message: ', msg);
+
+        socket.emit('chat received', 'coiso coisado');
     });
 });
 
